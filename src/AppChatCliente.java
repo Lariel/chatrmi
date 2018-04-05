1fa3d383e774d304aa74bb2493fe116a7de66948
12205112 <12205112@l1846799.portoalegre.pucrsnet.br>
2018-04-05 16:15:06 -0300

    Solucionado problema de comunicação com multiplos clientes; Servidor não estava recebendo IP por parâmetro, então aplicação funcionava somente com localhost, problema resolvido com a nova string do rebind

--- src/AppChatCliente.java
+++ src/AppChatCliente.java
@@ -7,6 +7,10 @@ public class AppChatCliente {
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
 		//String URLChatServidor="rmi://"+args[0]+"/ServidorChatRMI";
 		//IChatServidor servidor = (IChatServidor) Naming.lookup(URLChatServidor);
+		if(args.length < 2) {
+			System.err.println("Use java AppChatServidor <IP do Servidor> <Seu Nome>");
+            System.exit(1);
+		}
 		IChatServidor servidor = (IChatServidor) Naming.lookup("rmi://"+args[0]+"/ServidorChatRMI");
 		new Thread(new ChatCliente(args[1],servidor)).start();
 	}
--- src/AppChatServidor.java
+++ src/AppChatServidor.java
@@ -5,6 +5,11 @@ import java.rmi.RemoteException;
 public class AppChatServidor {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException {
-		Naming.rebind("ServidorChatRMI", new ChatServidor());
+		//Naming.rebind("ServidorChatRMI", new ChatServidor());
+		if(args.length < 1) {
+			System.err.println("Use java AppChatServidor <IP do Servidor>");
+            System.exit(1);
+		}
+		Naming.rebind("//"+args[0]+"/ServidorChatRMI", new ChatServidor());
 	}
 }
