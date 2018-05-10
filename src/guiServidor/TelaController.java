0a8efeba3a7058b1654b1b5e4c2f14ed6e31b614
12205112 <12205112@w1846773.portoalegre.pucrsnet.br>
2018-05-10 19:10:18 -0300

    organizado menus e add logo

--- src/guiServidor/TelaController.java
+++ src/guiServidor/TelaController.java
@@ -2,15 +2,38 @@ package guiServidor;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class TelaController implements Initializable{
+	
+	// Menu
+    @FXML // fx:id="miSobre"
+    private MenuItem miSobre; // Value injected by FXMLLoader
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
+	@FXML
+    void sobre(ActionEvent event) {
+    	Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Sobre");
+		alert.setHeaderText("TF - Sistemas Distribuidos");
+		alert.setContentText("Desenvolvido por: \n Ial Jonas\n Lariel Negreiros \n \nProfessor: \n Avelino Zorzo");
+		Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/WhatsLike.png")));
+		alert.showAndWait();
+    }
 	
 
 }
